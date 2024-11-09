5e2a6849873c4756a50253d92174e6fca09a6372
Sharath <sharaths21312@gmail.com>
2024-11-09 23:09:02 +0530

    line transitions working

--- ui-cooking/testui/src/routes/components/Lineplot.svelte
+++ ui-cooking/testui/src/routes/components/Lineplot.svelte
@@ -1,14 +1,73 @@
 <script lang="ts">
-    import * as d3 from 'd3'
-    const { line_data, header }: { line_data: [number, number][], header: string} = $props()
-    let linePlot: SVGElement
+    import * as d3 from "d3";
+    import { onMount } from "svelte";
+    const {
+        line_data,
+        header,
+    }: { line_data: [number, number][]; header: string } = $props();
+    let linePlot: SVGElement;
 
+    const height = 200;
+    const width = 300;
+    const lbord = 20;
+    const rbord = 20;
+    const tbord = 20;
+    const bbord = 20;
+    const w = width - lbord - rbord;
+    const h = height - tbord - bbord;
+    const xaxis = $derived(
+        d3
+            .scaleLinear()
+            .domain([0, 30])
+            .range([lbord, width - rbord]),
+    );
+    const yaxis = $derived(
+        d3
+            .scaleLinear()
+            .domain([0, line_data.length])
+            .range([height - bbord, tbord]),
+    );
+
+    onMount(() => {
+        $effect(() => {
+            let d3svg = d3.select(linePlot);
+            const line = d3
+                .line()
+                .x((d) => xaxis(d[0]))
+                .y((d) => yaxis(d[1]));
+            d3svg.selectChildren().remove();
+
+            const path = d3svg
+                .append("path")
+                .datum(line_data)
+                .attr("fill", "none")
+                .attr("stroke", "steelblue")
+                .attr("d", line).attr("opacity", 0);
+
+                requestAnimationFrame(() => {
+                const pathNode = path.node() as SVGPathElement
+                const pathLength = pathNode.getTotalLength()
+                console.log(path.node(), pathLength);
+                path.attr("opacity", 1)
+                .attr("stroke-dasharray", pathLength)
+                .attr("stroke-dashoffset", pathLength)
+                .transition().ease(d3.easeCubic).duration(2000).attr('stroke-dashoffset', 0);
     
+                d3svg
+                    .append("g")
+                    .attr("transform", `translate(0, ${height - bbord})`)
+                    .call(d3.axisBottom(xaxis).ticks(10));
+    
+                d3svg
+                    .append("g")
+                    .attr("transform", `translate(${lbord}, 0)`)
+                    .call(d3.axisLeft(yaxis).ticks(5));
+            })
+        });
+    });
 </script>
 
 <h2>{header}</h2>
 <div class="svg_container">
-    <svg viewBox="0 0 200 100" bind:this={linePlot}>
-
-    </svg>
+    <svg viewBox="0 0 300 200" bind:this={linePlot}> </svg>
 </div>
--- ui-cooking/testui/src/routes/shared/analytics.svelte
+++ ui-cooking/testui/src/routes/shared/analytics.svelte
@@ -3,6 +3,7 @@
     let { socket, visibility }: { socket: WebSocket, visibility: Boolean } = $props()
 
     import Heatmap from "../components/Heatmap.svelte";
+    import Lineplot from "../components/Lineplot.svelte";
     let heatmapdata: {"left-team": number[][], "right-team": number[][], "ball": number[][]} = $state({
         'left-team': [],
         'right-team': [],
@@ -26,9 +27,26 @@
     $effect(() => {
         if (visibility) {
             getHeatmapData()
+            // setlinedata()
         }
     })
 
+
+    let line_data: [number, number][] = $state([])
+    
+    function setlinedata() {
+        let min = 0
+        let max = 30
+        line_data=[]
+        for (let i = 0; i < max; i++) {
+            line_data.push([i, min + (Math.random()*(max-min))/2])
+    
+            const lastElement = line_data.at(-1);
+            if (lastElement) {
+                min = lastElement[1] as number;
+            }
+        }
+    }
     $inspect(heatmapdata)
 </script>
 
@@ -36,9 +54,10 @@
     <Heatmap header="Left team heatmap" heatmap_data={heatmapdata["left-team"]}/>
     <Heatmap header="Right team heatmap" heatmap_data={heatmapdata["right-team"]}/>
     <Heatmap header="Ball heatmap" heatmap_data={heatmapdata["ball"]}/>
+    <Lineplot header="Lineplot" {line_data} /> 
 </div>
 
-<!-- <button onclick={getHeatmapData} class="p-1 border-2 m-2 flex-shrink">Get heatmap data</button> -->
+<button onclick={() => {setlinedata(); getHeatmapData()}} class="p-1 border-2 m-2 flex-shrink">Get data</button>
 
 <style>
 </style>
