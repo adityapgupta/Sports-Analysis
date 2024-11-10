f377282aee0ae4391f23f19fba242b8cf3f73d9c
adityapgupta <adityapg@iisc.ac.in>
2024-11-11 01:32:29 +0530

    main edits

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ markers_path = 'results/trimmed/markers_trimmed.mp4'
 minimap_path = 'results/trimmed/minimap_trimmed.mp4'
 
 x, y = detections(clip_path, players_path, ball_path,
-                  return_class=False, verbose=True, project=False)
+                  return_class=True, verbose=True)
 
 with open(pkl_path, 'wb') as f:
     pickle.dump((x, y), f)
 
 draw_markers(clip_path, markers_path, x)
 
-# ball_data = minimap.ball_interpolate(y)
-# players_data = minimap.players_interpolate(y)
-# edges_data = minimap.edges_interpolate(y)
+ball_data = minimap.ball_interpolate(y)
+players_data = minimap.players_interpolate(y)
+edges_data = minimap.edges_interpolate(y)
 
 draw_minimap(ball_data, players_data, edges_data, minimap_path)
